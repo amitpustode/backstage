68c5ac0cc6a5a59be3fcd623ce322350d3af8d19
amitpustode <arpustode@gmail.com>
2025-02-04 07:29:12 +0000

    Saving local changes before rebase

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -51,6 +51,5 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
-backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
 backend.start();
