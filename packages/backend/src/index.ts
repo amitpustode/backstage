dd8ce05f646b10036eefdf940bff4289fefe2f1a
amitpustode <arpustode@gmail.com>
2025-02-04 09:36:26 +0000

    Saving local changes before rebase

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -10,6 +10,7 @@ import { createBackend } from '@backstage/backend-defaults';
 
 import kubernetes from './plugins/kubernetes';
 import { useHotMemoize } from '@backstage/backend-common';
+import { createEnv } from '@backstage/backend-common'; /
 const kubernetesEnv = useHotMemoize(module, () => createEnv('kubernetes'));
 apiRouter.use('/kubernetes', await kubernetes(kubernetesEnv));
 
