ba713dbc7baf9f52c21e0e3829893123ef9e24f6
amitpustode <arpustode@gmail.com>
2025-02-05 05:50:15 +0000

    Saving local changes before rebase

--- packages/backend/src/plugins/kubernetes.ts
+++ packages/backend/src/plugins/kubernetes.ts
@@ -1,29 +1,29 @@
-// // // packages/backend/src/plugins/kubernetes.ts
-// import { KubernetesBuilder } from '@backstage/plugin-kubernetes-backend';
-// import { Router } from 'express';
-// import { PluginEnvironment } from '../types';
-// import { CatalogClient } from '@backstage/catalog-client';
+// // packages/backend/src/plugins/kubernetes.ts
+import { KubernetesBuilder } from '@backstage/plugin-kubernetes-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
+import { CatalogClient } from '@backstage/catalog-client';
 
-// export default async function createPlugin(
-//   env: PluginEnvironment,
-// ): Promise<Router> {
-//   const catalogApi = new CatalogClient({ discoveryApi: env.discovery });
-//   const { router } = await KubernetesBuilder.createBuilder({
-//     logger: env.logger,
-//     config: env.config,
-//     catalogApi,
-//     permissions: env.permissions,
-//   }).build();
-//   return router;
-// }
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
+  const catalogApi = new CatalogClient({ discoveryApi: env.discovery });
+  const { router } = await KubernetesBuilder.createBuilder({
+    logger: env.logger,
+    config: env.config,
+    catalogApi,
+    permissions: env.permissions,
+  }).build();
+  return router;
+}
 
 // packages/backend/src/plugins/kubernetes.ts
-import { createRouter } from '@backstage/plugin-kubernetes-backend';
-import { PluginEnvironment } from '../types';
+// import { createRouter } from '@backstage/plugin-kubernetes-backend';
+// import { PluginEnvironment } from '../types';
 
-export default async function createPlugin({
-  logger,
-  config,
-}: PluginEnvironment) {
-  return await createRouter({ logger, config });
-}
+// export default async function createPlugin({
+//   logger,
+//   config,
+// }: PluginEnvironment) {
+//   return await createRouter({ logger, config });
+// }
