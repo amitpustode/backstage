70be8d208d760a0e99d309f923f2e0bac967a73e
amitpustode <arpustode@gmail.com>
2025-02-07 09:12:00 +0000

    Saving local changes 07-02-2025

--- packages/app/src/plugins.ts
+++ packages/app/src/plugins.ts
@@ -1,4 +1,4 @@
 // packages/app/src/plugins.ts
 export { argocdPlugin } from '@roadiehq/backstage-plugin-argo-cd';
-export { plugin as harbor } from '@bestsellerit/backstage-plugin-harbor';
-
+//export { plugin as harbor } from '@bestsellerit/backstage-plugin-harbor';
+export { plugin as harbor } from '@digitalist-open-cloud/backstage-plugin-harbor';
